37e4c03010875e6a673ca6ace8ff585362a5206b
우성환 <doqndnffo@gmail.com>
2020-01-16 22:21:58 +0900

    프로젝트 생성 및 Home 예제

---
+++ src/main/java/co/worker/board/home/Home.java
@@ -0,0 +1,12 @@
+package co.worker.board.home;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Home {
+    private String name;
+    private int age;
+}
---
+++ src/test/java/co/worker/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package co.worker.board;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
