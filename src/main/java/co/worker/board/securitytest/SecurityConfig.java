ec0261e46ace3e31518da3cec2c68a14c4991441
wsh0821 <wsh0821@station3.co.kr>
2020-02-25 19:02:13 +0900

    시큐리티 예제 적용

---
+++ src/main/java/co/worker/board/securitytest/SecurityConfig.java
@@ -0,0 +1,22 @@
+package co.worker.board.securitytest;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/", "/hello").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+}
